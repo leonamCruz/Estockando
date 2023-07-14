d1d78bb26d9b0a7444498b5738f42a41b051ba19
leonam <leonam447@gmail.com>
2023-07-14 18:18:38 -0300

    Slapsh Screen

--- app/src/main/java/tech/leonam/estockando/view/atividades/SplashScreen.kt
+++ app/src/main/java/tech/leonam/estockando/view/atividades/SplashScreen.kt
@@ -1,11 +1,13 @@
 package tech.leonam.estockando.view.atividades
 
+import android.annotation.SuppressLint
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import tech.leonam.estockando.databinding.ActivitySplashScreenBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +16,11 @@ class SplashScreen : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.logoSplash.alpha = 0f
-        binding.logoSplash.animate().setDuration(1500).alpha(1f).withEndAction{
+        binding.logoSplash.animate().setDuration(1200).alpha(1f).withEndAction{
             val intencao = Intent(this,BoasVindas::class.java)
             val options = ActivityOptions.makeCustomAnimation(
                 this,
-                android.R.anim.fade_in,
+                android.R.animator.fade_in,
                 android.R.anim.fade_out
             )
             startActivity(intencao, options.toBundle())
