d1f37aa409753869b4a04e493c516fe6374f669f
leonam <leonam447@gmail.com>
2023-07-16 12:42:09 -0300

    Cadastra Dao

---
+++ app/src/main/java/tech/leonam/estockando/model/CadastrarDAO.kt
@@ -0,0 +1,37 @@
+package tech.leonam.estockando.model
+
+import android.content.ContentValues
+import android.content.Context
+import tech.leonam.estockando.controller.Produtos
+
+class CadastrarDAO(
+    context: Context,
+    produto: Produtos
+) {
+    private var helper: Helper
+    private val pd = produto
+
+    init {
+        helper = Helper(context)
+    }
+
+    fun saveInDatabase() {
+        val db = helper.writableDatabase
+        val contentValues = ContentValues()
+        with(contentValues) {
+            with(pd) {
+                with(Helper) {
+                    put(COLUMN_NOME, nomeDoProduto)
+                    put(COLUMN_DESCRICAO, descricaoDoProduto)
+                    put(COLUMN_DATA_CADASTRO,dataCadastro)
+                    put(COLUMN_CODIGO_BARRAS,codigoDeBarras)
+                    put(COLUMN_IMAGEM,imagemDoProduto)
+                    put(COLUMN_PRECO,preco)
+                    put(COLUMN_QUANTIDADE,qntDoProduto)
+
+                    db.insert(NOME_TABELA, null,contentValues)
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/tech/leonam/estockando/model/Helper.kt
+++ app/src/main/java/tech/leonam/estockando/model/Helper.kt
@@ -4,23 +4,25 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class Helper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME,null, DATABASE_VERSION) {
+class Helper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
-        private const val DATABASE_NAME = "produtos.db"
+        private const val DATABASE_NAME = "inventario.db"
         private const val DATABASE_VERSION = 1
+        const val NOME_TABELA = "produtos"
+
+        const val TABLE_PRODUTOS = "produtos"
+        const val COLUMN_ID = "id"
+        const val COLUMN_NOME = "nome"
+        const val COLUMN_DESCRICAO = "descricao"
+        const val COLUMN_DATA_CADASTRO = "data_cadastro"
+        const val COLUMN_PRECO = "preco"
+        const val COLUMN_CODIGO_BARRAS = "codigo_barras"
+        const val COLUMN_IMAGEM = "imagem"
+        const val COLUMN_QUANTIDADE = "quantidade"
     }
 
     override fun onCreate(p0: SQLiteDatabase?) {
-        val sql = "CREATE TABLE IF NOT EXISTS produtos (\n" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
-                "nome TEXT,\n" +
-                "descricao TEXT,\n" +
-                "dataCadastro TEXT,\n" +
-                "preco TEXT,\n" +
-                "codigoDeBarras TEXT,\n" +
-                "imagemDoProduto TEXT,\n" +
-                "qntDoProduto TEXT\n" +
-                ")"
+        val sql = "CREATE TABLE IF NOT EXISTS produtos ($COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, $COLUMN_NOME TEXT, $COLUMN_DESCRICAO TEXT, $COLUMN_DATA_CADASTRO TEXT, $COLUMN_PRECO TEXT, $COLUMN_CODIGO_BARRAS TEXT, $COLUMN_IMAGEM TEXT, $COLUMN_QUANTIDADE TEXT)"
         p0?.execSQL(sql)
     }
 
