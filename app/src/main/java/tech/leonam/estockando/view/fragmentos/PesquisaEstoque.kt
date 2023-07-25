fc2a6e662f72dcf21638c8b0b04ae69e91574fe7
leonam <leonam447@gmail.com>
2023-07-25 15:42:55 -0300

    Pesquisa por id ok

--- app/src/main/java/tech/leonam/estockando/view/fragmentos/PesquisaEstoque.kt
+++ app/src/main/java/tech/leonam/estockando/view/fragmentos/PesquisaEstoque.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import tech.leonam.estockando.R
 import tech.leonam.estockando.databinding.FragmentPesquisaEstoqueBinding
 import tech.leonam.estockando.view.reciclaveis.RecyclerViewClasse
@@ -27,8 +28,20 @@ class PesquisaEstoque : Fragment() {
         binding.cardPesquisaPorId.setOnClickListener {
             val intent = Intent(context, RecyclerViewClasse::class.java)
             intent.putExtra("opcao", 1)
-            intent.putExtra("id",binding.numeroDoIdPesquisa.text.toString())
-            startActivity(intent)
+            val id = binding.numeroDoIdPesquisa.text.toString()
+            if (id.isNotBlank()) {
+                intent.putExtra("id", id)
+                startActivity(intent)
+            } else {
+                val snackbar = Snackbar.make(
+                    binding.root,
+                    getString(R.string.insira_um_valor),
+                    Snackbar.LENGTH_SHORT
+                )
+                snackbar.setTextColor(requireContext().getColor(R.color.branco))
+                snackbar.setBackgroundTint(requireContext().getColor(R.color.preto))
+                snackbar.show()
+            }
         }
         return view
     }
